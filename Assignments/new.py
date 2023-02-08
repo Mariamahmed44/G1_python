acaaac1ddf13fb6d8025cd0806831325fb11c9af
noran97 <99215076+noran97@users.noreply.github.com>
2023-02-08 22:51:34 +0200

    first assignments

---
+++ Assignments/new.py
@@ -0,0 +1,32 @@
+
+#no.6
+namme="mariam"
+print(namme[1])
+print(namme[2])
+print(namme[-1])
+#no.7
+print(namme[1:4])
+print(namme[0: :2])
+#no.8
+name = "#@#@Ali#@#@"
+print(name.strip("#@"))
+#no.9
+num=input("enter number ")
+print(num.zfill(4))
+#no.10
+name_one="ali"
+print(name_one.rjust(20,"@"))
+#no.11
+nam="NoRan"
+print(nam.swapcase())
+#no.12
+msg=" i love python and learning python"
+print(msg.count("python"))
+#no.13
+a="school"
+print(a.index("l"))
+#no.14
+naame="nor"
+age=20
+country="egypt"
+print("my name is : {:s} my age: {:d} and my country is{:s}".format(naame,age,country))
